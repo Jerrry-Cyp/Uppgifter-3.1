41cdb8e394e1221b71c7035a29c6ad2386a3a53f
alsa07007 <alsa07007@E-PW05CGR5>
2024-11-13 08:34:30 +0100

    Add project files.

---
+++ Uppgifter 3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgifter_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv din ålder:");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder < 19 && ålder > 16) Console.WriteLine("Du kan delta i tävlingen");
+            if (ålder < 16) Console.WriteLine("Du är tyvär för ung för att delta i tävlingen");
+            if (ålder > 19) Console.WriteLine("Du är för gammal för att delta i tävlingen");
+        }
+    }
+}
